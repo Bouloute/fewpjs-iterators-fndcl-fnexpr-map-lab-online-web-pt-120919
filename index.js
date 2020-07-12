d50d13bed0556703423e88175ac6d657f59adbc8
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-07-12 22:15:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ const titleCased = () => {
 
 function capitalize(sentence){
   let words = sentence.split(' ')
-  (words.map(word => word.charAt(0).toUpperCase() + word.slice(1))).join(" ")
+  return (words.map(word => word.charAt(0).toUpperCase() + word.slice(1))).join(" ")
   
 }
